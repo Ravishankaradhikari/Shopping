8719c8b7e3aae55a6d6add7db49dbc256cadfa90
Ravishankaradhikari <ravishankaradhikari10@gmail.com>
2023-09-02 00:32:13 +0545

    ravi

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Navigation from './components/Navigation'
+import HeroSection from './components/HeroSection'
+
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  )
+}
+
+export default App
